822ba8387be1953570c0efc023014ac835b4a229
Jan Hülsmann <mail@janhuelsmann.com>
2022-11-27 12:56:32 +0200

    Adjust permissions view

--- SaneSideButtons/PermissionView.swift
+++ SaneSideButtons/PermissionView.swift
@@ -18,21 +18,21 @@ struct PermissionView: View {
                     .font(.title)
                     .fontWeight(.medium)
                 Text("We'll have you up and running in just a minute!")
-                    .font(.subheadline)
+                    .font(.body)
                     .fontWeight(.medium)
-            }.padding(.vertical, 10)
-            ZStack {
-                RoundedRectangle(cornerRadius: 5, style: .continuous)
-                    .frame(width: 600, height: 200)
-                    .foregroundColor(Color("BackgroundColor"))
-                    .padding(10)
-                    .padding(.bottom, 20)
+            }
+            .padding(.vertical, 10)
+            ZStack(alignment: .top) {
+                Rectangle()
+                    .fill(Color("BackgroundColor"))
+                    .frame(width: 600, height: 240)
                 PermissionContentView()
+                    .padding(.vertical, 20)
             }
 
         }
         .background(.thinMaterial)
-        .onReceive(timer) { _ in
+        .onReceive(self.timer) { _ in
             self.pollPermissions()
         }
     }
@@ -48,58 +48,88 @@ struct PermissionView: View {
 }
 
 struct PermissionContentView: View {
+    @Environment(\.openURL) private var openURL
+
+    private let settingsString: String = "x-apple.systempreferences:com.apple.preference"
+    private let privacyString: String = ".security?Privacy"
+
     var body: some View {
         VStack {
             // swiftlint:disable line_length
             Text("SaneSideButtons needs your permission to detect mouse events and trigger actions in applications. Follow these steps to authorize it:")
             // swiftlint:enable line_length
-                .font(.subheadline)
+                .font(.body)
                 .multilineTextAlignment(.center)
                 .padding(.horizontal, 40)
                 .padding(.bottom, 30)
-            HStack(alignment: .top) {
-                VStack {
+            Grid {
+                GridRow {
                     Image(systemName: "1.circle.fill")
                         .font(.title)
                         .foregroundColor(Color.gray)
-                    Text("Go to System Settings")
-                        .font(.callout)
-                        .padding(7)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 16)
-                                .stroke(.blue, lineWidth: 1)
-                        )
-                        .padding(.top, 30)
-                }.padding(.horizontal, 5)
-
-                VStack {
                     Image(systemName: "2.circle.fill")
                         .font(.title)
                         .foregroundColor(Color.gray)
-                    Text("Privacy & Security")
-                        .font(.callout)
-                        .padding(7)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 16)
-                                .stroke(.blue, lineWidth: 1)
-                        )
-                        .padding(.top, 30)
-                }.padding(.horizontal, 5)
-
-                VStack {
                     Image(systemName: "3.circle.fill")
                         .font(.title)
                         .foregroundColor(Color.gray)
-                    Text("Add SaneSideButtons to Accessibility & Input Monitoring")
-                        .font(.callout)
-                        .multilineTextAlignment(.center)
-                        .padding(7)
-                        .overlay(
+                }
+                GridRow {
+                    Button {
+                        if let url = URL(string: self.settingsString) {
+                            self.openURL(url)
+                        }
+                    } label: {
+                        HStack {
+                            Image(systemName: "gear")
+                            Text("Go to System Settings")
+                                .font(.body)
+                        }
+                        .padding(10)
+                        .background(
                             RoundedRectangle(cornerRadius: 16)
-                                .stroke(.blue, lineWidth: 1)
+                                .fill(.thinMaterial)
+                                .shadow(radius: 2)
                         )
-                        .padding(.top, 15)
-                }.padding(.horizontal, 5)
+                    }.buttonStyle(.plain)
+
+                    Button {
+                        if let url = URL(string: self.settingsString + self.privacyString) {
+                            self.openURL(url)
+                        }
+                    } label: {
+                        HStack {
+                            Image(systemName: "hand.raised.fill")
+                            Text("Privacy & Security")
+                                .font(.body)
+                        }
+                        .padding(10)
+                        .background(
+                            RoundedRectangle(cornerRadius: 16)
+                                .fill(.thinMaterial)
+                                .shadow(radius: 2)
+                        )
+                    }.buttonStyle(.plain)
+
+                    Button {
+                        if let url = URL(string: self.settingsString + self.privacyString) {
+                            self.openURL(url)
+                        }
+                    } label: {
+                        HStack {
+                            Image(systemName: "keyboard.badge.eye.fill")
+                            Text("Add SaneSideButtons to Accessibility & Input Monitoring")
+                                .font(.body)
+                                .multilineTextAlignment(.center)
+                        }
+                        .padding(10)
+                        .background(
+                            RoundedRectangle(cornerRadius: 24)
+                                .fill(.thinMaterial)
+                                .shadow(radius: 2)
+                        )
+                    }.buttonStyle(.plain)
+                }.frame(width: 180, height: 90)
             }
         }
     }
